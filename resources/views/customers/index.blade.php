5fa9f5615bc04d2ba1d784ddc1a577b627dac73f
Augustino Emanuel <augustino.emanuel73@gmail.com>
2020-08-17 19:55:39 +0300

    customer

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['activePage' => 'table', 'titlePage' => __('Table List')])
+@extends('layouts.app', ['activePage' => 'Customers', 'titlePage' => __('Table List')])
 
 @section('content')
 <div class="content">
